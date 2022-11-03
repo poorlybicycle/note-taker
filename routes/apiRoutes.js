2e4fae4ce3c3718f68f32dd0260bc84b135999c4
Mime Jr <petravidovic8@gmail.com>
2022-11-02 22:07:52 -0400

    fix routes in html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,17 @@ const path = require('path');
 // };
 
 module.exports = (app) => {
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-  })};
+  app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'),
+      (err, data) => {
+        if (err) throw err
+        res.json(JSON.parse(data))
+      }
+    )
+  })
+  app.post('/api/notes', (req, res) => {
+    console.log(req.body)
+
+  }
+  )
+};
--- server.js
+++ server.js
@@ -8,8 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-require('./routes/htmlRoutes')(app);
+
 require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
